2e3d790e4cf4d6f5f8b38743563f8091d6fca6fd
andruw159 <andres.bernalm@udea.edu.co>
2023-10-15 19:58:42 -0500

    Clase Jugador y borrador de Frontend
    Se hizo la clase jugador con los constructores (hay que probar si funciona). Se agregaron 6 TextBox como propuesta para agregar datos.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 83);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(345, 28);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(40, 143);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(345, 28);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(40, 204);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(345, 28);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(430, 204);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(345, 28);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(430, 143);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(345, 28);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(430, 83);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(345, 28);
+            this.textBox6.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
--- Jugador.cs
+++ Jugador.cs
@@ -8,5 +8,73 @@ namespace Lab3
 {
     internal class Jugador
     {
+        String nombre;
+        String correo;
+        String endulzadaIdeal;
+        String regaloIdeal;
+        String amigoSecreto;
+
+        public Jugador(String nombre, String correo, String endulzadaIdeal, String regaloIdeal)
+        {
+            this.nombre = nombre;
+            this.correo = correo;
+            this.endulzadaIdeal = endulzadaIdeal;
+            this.regaloIdeal = regaloIdeal;
+        }
+
+        public String getNombre()
+        {
+            return nombre;
+        }
+
+        public String getCorreo()
+        {
+            return correo;
+        }
+        public String getEndulzadaIdeal()
+        {
+            return endulzadaIdeal;
+        }
+
+        public String getRegaloIdeal()
+        {
+            return regaloIdeal;
+        }
+
+        public String getAmigoSecreto()
+        {
+            return amigoSecreto;
+        }
+
+        public void setNombre(String nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void setCorreo(String correo)
+        {
+            this.correo = correo;
+        }
+
+        public void setEndulzadaIdeal(String endulzadaIdeal)
+        {
+            this.endulzadaIdeal = endulzadaIdeal;
+        }
+
+        public void setRegaloIdeal(String regaloIdeal)
+        {
+            this.regaloIdeal = regaloIdeal;
+        }
+
+        public void setAmigoSecreto(String amigo)
+        {
+            this.amigoSecreto = amigo;
+        }
+
+        public String mostrarJugador()
+        {
+            return "Jugador: " + this.nombre + ", " + this.correo + ", " + this.endulzadaIdeal + ", " + this.regaloIdeal;
+        }
     }
 }
+
